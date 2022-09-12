5b5697b2a57fad4e2e81be238999d3d2a6a95205
Ayush Verma <ayush7607verma@gmail.com>
2022-09-12 21:49:18 +0530

    day-2 completed

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_embedded/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
