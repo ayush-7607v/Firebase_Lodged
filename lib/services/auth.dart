ce7373d84b11d805546509fdac5a33f41d92e0b6
Ayush Verma <ayush7607verma@gmail.com>
2022-09-13 13:24:11 +0530

    day 3 donee...hehe boii

---
+++ lib/services/auth.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+
+final FirebaseAuth _authvar= FirebaseAuth.instance;
+
+
+Future signInAnon() async{
+
+ try {
+         UserCredential result = await _authvar.signInAnonymously();
+         User? user_var = result.user;
+         
+// ............................posiible err0r....................................
+         
+         return user_var;
+    } 
+
+// if this error give then below statement run
+    catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+// The classes have been renamed. 'AuthResult' has been renamed to 'UserCredential' and 'FirebaseUser' is renamed to 'User'
+
+
+
+
+
+}
+
+
+
+}
